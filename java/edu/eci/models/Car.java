40586d3c34dc7932acea47ba04942b17fd950be8
italoNovoa <italolo97@hotmail.com>
2019-03-24 14:44:11 -0500

    user

---
+++ java/edu/eci/models/Car.java
@@ -0,0 +1,33 @@
+package edu.eci.models;
+
+public class Car {
+
+    private String licencePlate;
+    private String brand;    
+    
+    
+	public Car() {
+		
+	}
+
+	public Car(String licencePlate, String brand) {
+		this.licencePlate = licencePlate;
+		this.brand = brand;
+	}
+	
+	public String getLicencePlate() {
+		return licencePlate;
+	}
+	public void setLicencePlate(String licencePlate) {
+		this.licencePlate = licencePlate;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+    
+    
+}
---
+++ java/edu/eci/services/UserServices.java
@@ -0,0 +1,73 @@
+package edu.eci.services;
+
+import edu.eci.models.User;
+import edu.eci.persistences.repositories.IUserRepository;
+import edu.eci.services.contracts.IUserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class UserServices implements IUserServices{
+
+    @Autowired
+    @Qualifier("UserMemoryRepository")
+    private IUserRepository userRepository;
+
+    @Override
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User create(User user) {
+        if(null == user.getId())
+            throw new RuntimeException("Id invalid");
+        else if(userRepository.find(user.getId()) != null)
+            throw new RuntimeException("The user exists");
+        else
+            userRepository.save(user);
+        return user;
+    }
+
+    @Override
+    public User get(UUID id) {
+        return userRepository.find(id);
+    }
+
+    @Override
+    public User get(String name) {
+        return userRepository.getUserByUserName(name);
+    }
+
+    @Override
+	public User updateUser(User user) {
+		if(null == user.getId())
+            throw new RuntimeException("Id invalid");
+        else if(userRepository.find(user.getId()) == null)
+        	userRepository.save(user);            
+        else {
+        	User us1=userRepository.find(user.getId());
+        	us1.setName(user.getName());
+        	userRepository.update(us1);
+        	return us1;	
+        }
+        return user;		
+	}
+
+	@Override
+	public User delete(UUID id) {
+		if(null == id)
+            throw new RuntimeException("Id invalid");
+        else if(userRepository.find(id) == null)
+        	throw new RuntimeException("Usuario no se encuentra");          
+        else {
+        	User us1=userRepository.find(id);
+        	userRepository.delete(us1);
+        	return us1;	
+        }
+	}
+}
